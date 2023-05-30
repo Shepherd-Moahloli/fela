a4b2f1d0b7997ee58cb3799dd6a18c91270d87b9
Earl <shepherdmoahloli122@gmail.com>
2023-05-30 11:08:30 +0200

    Still working on the process

--- js/main.js
+++ js/main.js
@@ -3,6 +3,33 @@ async function getStoko() {
   const stoko = await data.json();
 
   console.log(stoko);
+
+  stoko.forEach((group) => {
+    console.log(group);
+  });
+
+  const itemHTML = `
+  <li>
+            <h3 class="name">RICE</h3>
+            <a href="#starch-rice" class="picture">
+              <img
+                src="https://picsum.photos/seed/rice/200/200"
+                width="200px"
+                height="200px"
+              />
+            </a>
+            <span class="price">R12.00</span>
+            <span class="quantity">100g</span>
+          </li>`;
+  const groupHTML = `
+  <div class="food-group">
+        <ul>
+         ${itemHTML}
+          </ul>
+          </div>
+          `;
+
+  console.log(itemHTML);
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
